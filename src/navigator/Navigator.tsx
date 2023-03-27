ad7b90b37d74b21a95f1ee9d0f3ae6ffcec16004
Gerardo Hernandez <hernandez.gerardo@bcgplatinion.com>
2023-03-26 21:21:32 -0300

    install react navigator and create structure folders

---
+++ src/navigator/Navigator.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+export const Navigator = () => {
+  return (
+    <Stack.Navigator>
+      {/* <Stack.Screen name="Home" component={Home} /> */}
+    </Stack.Navigator>
+  );
+};
